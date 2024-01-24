3b1aa7f4e4d036083831c03588a3a7957993f5f6
feds-agent <137819891+feds-agent@users.noreply.github.com>
2024-01-24 06:18:49 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
-    });
-});
+document.addEventListener('DOMContentLoaded', function () {
+    const hiddenElements = document.querySelectorAll('.hidden');
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        });
+    }, { threshold: 0.5 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+    hiddenElements.forEach((el) => observer.observe(el));
+
+    const intro = document.querySelector('.intro');
+    const introText = document.querySelector('.intro-text');
+
+    function hideIntro() {
+        intro.style.display = 'none';
+    }
+
+    introText.style.opacity = 1;
+    introText.style.transform = 'translateY(0)';
+
+    setTimeout(function () {
+        introText.style.animation = 'flames 2s ease-in-out forwards';
+        setTimeout(hideIntro, 2000);
+    }, 5000);
+});
